9579a256d421df824343bc14adb10e644fad1b26
hapticfish <jquinmail@gmai.com>
2018-08-06 18:02:20 -0500

    Update with ticker .1

---
+++ CoinStryker.Com/index.php
@@ -0,0 +1,280 @@
+<?php>
+//get PHP working and also figure out Mamp how to work with brackets
+$url = "https://www.bitstamp.net/api/ticker/";
+$fgc = file_get_contents($url);
+$json = json_decode(fgc, true);
+
+$price = $json["last"];
+$high = $json["high"];
+$low = $json["low"];
+$date = date("m-d-Y - h:i:sa");
+$open =$json["open"];
+
+if(open < $price){
+//price went up
+    $indicator = "+";
+    $change = $price - $open;
+    $percent = $change / $open;
+    $percent = $percent * 100;
+    percentChange = $indicator.number_format($percent, 2);
+    $color = "green";
+}
+
+if(open > $price){
+//price went down
+    $indicator = "-";
+    $change = $open - $price;
+    $percent = $change / $open;
+    $percent = $percent * 100;
+    percentChange = $indicator.number_format($percent, 2);
+    $color = "red";
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Bitcoin Stryker.com</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/custom.css" rel="stylesheet">
+      
+    <!-- fontawsome.com -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.1/css/all.css" integrity="sha384-O8whS3fhG2OnA5Kas0Y9l3cfpmYjapjI0E4theH4iuMD+pLhbf6JI0jIMfYcK3yZ" crossorigin="anonymous">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+      
+      <nav class="navbar navbar-fixed-top navbar-inverse">
+  <div class="container">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">Brand</a>
+        
+      
+        
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav navbar">
+        <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
+        <li><a href="#">Link</a></li>
+         <li><a href="#">Link</a></li>
+        
+      </ul>
+
+        
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#">Link</a></li>
+        <li><a href="#">Link</a></li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Resources <span class="caret"></span></a>
+            
+          <ul class="dropdown-menu">
+            
+            <li><a href="#">Action</a></li>
+            <li><a href="#">Another action</a></li>
+            <li><a href="#">Something else here</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#">About Us</a></li>
+          </ul>
+        </li>
+      </ul>
+        
+        <div id="TickContainer">
+          <table width="100%">
+             <tr>
+                <td rowspan="3" width="60%" id="lastPrice"><?php echo $price; ?></td>
+                <td align="right" style="color: <?php echo $color; ?>"><?php echo $percentChange; ?></td>  
+             </tr>
+              <tr>
+                <td align="right"><?php echo $high; ?></td>
+              </tr>
+              <tr>
+                <td align="right"><?php echo $low; ?></td>
+              </tr>
+              <tr>
+                  <td colspan="2" align="right" id="timeDate"><?php echo $date; ?>
+                  </td>
+              </tr>
+          </table>
+      </div>
+        
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+
+
+
+<div class="body_content">
+<div class="container">
+    <div class="row">
+        <div class="col-md-10">
+                <div class="jumbotron text-center">
+              <h1>Hello, world!</h1>
+              <p>...</p>
+              <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p>
+            </div>
+            <hr>
+        </div> 
+
+        <div class="col-xs-2">
+            ADDs area
+        </div>
+        
+        <div class="col-md-6">
+            <div class="jumbotron">
+              <h1>Hello, world!</h1>
+              <p>...</p>
+              <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p>
+            </div>
+        </div>
+        
+        <div class="col-md-4">
+            <div class="jumbotron">
+              <h1>Hello, world!</h1>
+              <p>...</p>
+              <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p>
+            </div>
+        </div>
+        
+        <div class="col-md-2">
+            ADDs area
+        </div>
+    </div>
+</div>
+
+<!--CONTINUE TO MAKE BLOGGING AREA, NEEDS WORKIGN ARE ALONG SIDE ADDS AND SUCH-->
+<div class="container blog_container">
+    <div class="row">
+        <div class="col-md-10">
+            <div class="panel panel-default">
+                <div class="panel-heading"><h2 class="panel-title">This is the Heading of the pannel</h2></div>
+                    <div class="panel-body">
+                        This is some of the content that is in the website
+                    </div>
+                    <table class="table">
+                        <tr>
+                            <th>Heading 1</th>
+                            <th>Heading 1</th>
+                            <th>Heading 1</th>
+                        </tr>
+                        <tr>
+                            <td>content</td>
+                            <td>content</td>
+                            <td>content</td>
+                        </tr>
+                        <tr>
+                            <td>content</td>
+                            <td>content</td>
+                            <td>content</td>
+                        </tr>
+                    </table>
+                
+                    <li class="list-group-item">Item</li>
+                    <li class="list-group-item">Item</li>
+                    <li class="list-group-item">Item</li>
+                    <li class="list-group-item">Item</li>
+                    <li class="list-group-item">Item</li>
+                
+                <div class="panel-footer">This is the footer for credits/tags/and author info
+                </div>
+            </div>
+        </div>
+    </div>
+    
+
+</div>
+
+</div><!--body_content end tag-->    
+
+<div class="navbar navbar-inverse navbar-fixed-bottom" role="navigation">
+    <div class="container">
+        <div class="navbar-text pull-left">
+            <p>&copy; 2018 BitcoinStryker.Com</p>
+        </div>
+        
+        <div class="navbar-text pull-right">
+            <a href="#"><i class="fab fa-facebook-square fa-2x"></i></a>
+            <a href="#"><i class="fab fa-twitter fa-2x"></i></a>
+            <a href="#"><i class="fab fa-google-plus fa-2x"></i></a>
+        </div>
+        
+         <div class="navbar-text pull-right">
+            <li><a href="#contact" data-toggle="modal">Message Us</a></li>
+        </div>
+        
+        <div class="navbar-text pull-right">
+            <li><a href="#contact" data-toggle="modal">About Us</a></li>
+        </div>
+    </div>
+</div>
+      
+      
+<div class="modal fade" id="contact" role="dialog">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <form class="form-horizontal">
+                        <div class="modal-header">
+                            <h4>Message Us</h4>
+                        </div>
+                        <div class="modal-body">
+                        
+                            <div class="form-group">
+                            <label for="contact-name" class="col-lg-2 control-lable">Name:</label>
+                            <div class="col-lg-10">
+                                <input type="text" class="form-control" id="contact-name" placeholder="Full Name">    
+                            </div>
+                            </div>
+                            
+                            <div class="form-group">
+                            <label for="contact-email" class="col-lg-2 control-lable">Email</label>
+                            <div class="col-lg-10">
+                                <input type="email" class="form-control" id="contact-email" placeholder="you@example.com">    
+                            </div>
+                            </div>
+                            
+                            <div class="form-group">
+                            <label for="contact-msg" class="col-lg-2 control-lable">Message:</label>
+                            <div class="col-lg-10">
+                                <textarea class="form-control" rows="8"></textarea>    
+                            </div>
+                            </div>
+                            
+                        </div>
+                        <div class="modal-footer">
+                            <button class="btn btn-submit" type="submit">Submit</button>
+                            <a class="btn btn-default" data-dismiss="modal">Close</a>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div><!--end of modal-->
+
+      
+      
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
